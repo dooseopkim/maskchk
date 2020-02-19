0ba8f7dba0ec6d5f62bfb3376a606bede6cdabee
dooseopkim <entjqcjswo@gmail.com>
2020-02-19 13:30:51 +0900

    Refactoring..

--- app.py
+++ app.py
@@ -1,23 +1,21 @@
-import os, sys, logging, io
-from logging import handlers
-from configparser import ConfigParser
-from requests import Session
+import os
+import sys
+import io
+import requests
+import logging, logging.handlers
+
 from bs4 import BeautifulSoup
-from selenium import webdriver
+from configparser import ConfigParser
+
 
 class App:
 
-    MSG_FORM = "\n지금 살 수 있다고 합니다~\n▶ {dsc}\n▶ 가격 : {price}\n▶ 바로가기 : {url}"
 
     def __init__(self, WD):
         self.WD = WD
-        self._logger = self.initLogger()
+        self._logger = None
         self._conf = None
         self._items = None
-
-        self._logger.info('Start MaskBot !!!')
-
-
 
 
     # - 품절 체크
@@ -51,20 +49,20 @@ class App:
         headers = {
             "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
         }
-        response = Session().get(url, headers=headers)
+        response = requests.get(url, headers=headers)
         if response.status_code != 200:
             raise Exception('Something Wrong your image..')
 
         return response.content
+
     # - 메세지 포매팅 후 반환
     @staticmethod
     def _msg(dsc=None, price=None, url=None):
-        return App.MSG_FORM.format(dsc=dsc, price=price, url=url)
-
+        return "▶ {dsc}\n▶ 가격 : {price}\n▶ 바로가기\n{url}".format(dsc=dsc, price=price, url=url)
 
 
     # - 로거 초기화
-    def initLogger(self):
+    def _initLogger(self):
         log_dir = os.path.join(self.WD, 'logs')
         if not os.path.exists(log_dir):
             os.makedirs(log_dir)
@@ -72,13 +70,17 @@ class App:
         logger = logging.getLogger('MaskBot')
         logger.setLevel(logging.INFO)
         fmt = logging.Formatter('%(asctime)s - [%(levelname)s|%(filename)s:%(lineno)s] - %(message)s')
+
         st_handler = logging.StreamHandler()
-        f_handler = handlers.RotatingFileHandler(os.path.join(log_dir, 'maskchk.log'), 'a', 10 * 1024 * 1024, 5)
+        f_handler = logging.handlers.RotatingFileHandler(os.path.join(log_dir, 'maskchk.log'), 'a', 10 * 1024 * 1024, 5)
+
         st_handler.setFormatter(fmt)
         f_handler.setFormatter(fmt)
+
         logger.addHandler(st_handler)
         logger.addHandler(f_handler)
-        return logger
+
+        self._logger = logger
 
     # - 환경설정 파일 초기화
     def _initConf(self, file_name='app.conf'):
@@ -89,67 +91,63 @@ class App:
 
     # - 짧은 URL로 변환
     def _shortURL(self, url):
-        if not self._conf:
-            self._initConf()
+        try:
+            NAVER = self._conf['naverAPI']
+            response = requests.post(
+                NAVER['URL'],
+                headers={
+                    'X-Naver-Client-Id': NAVER['clientID'],
+                    'X-Naver-Client-Secret': NAVER['clientSecret']
+                },
+                data={
+                    'url': url.encode('utf-8')
+                }
+            )
 
-        NAVER = self._conf['naverAPI']
-        response = Session().post(
-            NAVER['URL'],
-            headers={
-                'X-Naver-Client-Id': NAVER['clientID'],
-                'X-Naver-Client-Secret': NAVER['clientSecret']
-            },
-            data={
-                'url': url.encode('utf-8')
-            }
-        )
+            if response.json()['code'] != '200':
+                raise Exception('Trans short url is fail..')
 
-        if response.json()['code'] != '200':
-            self._logger.warning('Trans short url is fail..')
+            return response.json()['result']['url']
+
+        except Exception as e:
+            self._logger.warning(e)
             return url
 
-        return response.json()['result']['url']
 
     # - 라인 Notify 메세지 전송
     def _sendNotify(self, item):
-        if not self._conf:
-            self._initConf()
+        try:
+            url = self._conf['default']['HOST'] + item['link']  # original URL
+            message = self._msg(item['dsc'], item['price'], self._shortURL(url))  # message
 
-        url = self._conf['default']['HOST'] + item['link']  # original URL
-        message = self._msg(item['dsc'], item['price'], self._shortURL(url))  # message
+            # img = self._conf['default']['HOST'] + item['img']  # image URL
+            imageFile = io.BytesIO(self._getPhoto(item['img']))
 
-        img = self._conf['default']['HOST'] + item['img']  # image URL
-        imageFile = io.BytesIO(self._getPhoto(img))
+            TARGET_URL = self._conf['notify']['URL']
+            TOKEN = self._conf['notify']['TOKEN']
 
-        TARGET_URL = self._conf['notify']['URL']
-        TOKEN = self._conf['notify']['TOKEN']
+            headers = {'Authorization': 'Bearer {TOKEN}'.format(TOKEN=TOKEN)}
+            data = {'message': message}
+            files = {'imageFile': imageFile}
 
-        headers = {'Authorization': 'Bearer {TOKEN}'.format(TOKEN=TOKEN)}
-        data = {'message': message}
-        files = {'imageFile': imageFile}
-
-        response = Session().post(TARGET_URL, headers=headers, data=data, files=files)
-
-
-
+            response = requests.post(TARGET_URL, headers=headers, data=data, files=files)
+        except Exception as e:
+            self._logger.error(e)
 
     # - 크롤링
     def _crawl(self):
-        if not self._conf:
-            self._initConf()
-
         try:
-            url = self._conf['default']['url']
+            TARGET_URL = self._conf['default']['url']
             headers={
                 "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36"
             }
 
-            session = Session().get(url, headers=headers)
-            session.encoding = None # ISO-8859-1 (euc-kr)
-            if session.status_code != 200:
+            response = requests.get(TARGET_URL, headers=headers)
+            response.encoding = None # ISO-8859-1 (euc-kr)
+            if response.status_code != 200:
                 raise Exception("Wrong page.. Check your URL")
 
-            html = session.text
+            html = response.text
             soup = BeautifulSoup(html, 'lxml')
             self._items = App._extractItems(soup)
 
@@ -158,17 +156,26 @@ class App:
 
 
     def run(self):
-        # Crawling
-        if not self._items:
-            self._crawl()
+        # List of message for send notification
+        messages = []
 
-        # Check soldout
-        for item in self._items:
-            if self._isSoldOut(item):
-                continue
-            else:
-                selling = self._parse(item)
-                self._sendNotify(selling)
+        # Initialize
+        self._initLogger()
+        self._initConf()
+
+        # Crawling
+        self._crawl()
+
+        # Check Soldout
+        selling_items = [item for item in self._items if not self._isSoldOut(item)]
+        print(selling_items)
+        # # Check soldout
+        # for item in self._items:
+        #     if self._isSoldOut(item):
+        #         continue
+        #     else:
+        #         selling = self._parse(item)
+        #         self._sendNotify(selling)
 
 
 if __name__ == '__main__':
